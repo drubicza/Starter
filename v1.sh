27c01eb67d202e7f4dacf5c032439b845d721020
drubicza <id.slacker@gmail.com>
2019-09-07 06:25:29 +0800

    Add decompiled source

--- v1.sh
+++ v1.sh
@@ -1,20 +1,11 @@
 termux-setup-storage
 cd  /data/data/com.termux/files/usr/etc
-
 rm bash.bashrc
-
 ls
-
 cd $HOME
-
 ls
-
 cd Starter
-
 ls
-
 cp bash.bashrc /data/data/com.termux/files/usr/etc
-
 ls
-
 cd $HOME
